d0a43ac1ecb8407498b7c94178f53b1bf762e0c7
Prem-choudhary <pk5480524@gmail.com>
2026-05-19 14:15:06 +0530

    some change in this code

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+// let h2 = document.querySelector("h2")
+// console.dir(h2.innerText)
+
+// // console.log(h2 + "Heyyy Mishu")
+// h2.innerText= h2.innerText + " Hey Mishu"
+
+let Divs = document.querySelectorAll(".box")
+console.log(Divs)
---
+++ callBack/script.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  console.log("Hello World");
+}, 2000);
+
+
+console.log("Hello")
+
+
